3d3accece730a31a6cb99e094e25d6cae13560a1
lidorav <38429030+lidorav@users.noreply.github.com>
2019-05-01 18:56:05 +0300

    lots of work

--- js/site.js
+++ js/site.js
@@ -1,3 +1,7 @@
+
+//dictionary for regristed users 
+var logins = [{key:'a', value:'a'}];
+
 $(document).ready(function() {
 
     $('#datepicker').datepicker({
@@ -10,4 +14,83 @@ $(document).ready(function() {
     }).on('changeDate', function(selected) {
       var minDate = new Date(selected.date.valueOf());
     });
+
+    $(".switchdiv").on('click',function(e){
+      console.log(e)
+      $(".welcome").attr('hidden',true)
+      $(".Sign-Up").attr('hidden',true)
+      $(".limiter").attr('hidden',true)
+    if(e.currentTarget.text == "Welcome")
+    $(".welcome").removeAttr('hidden')
+    if(e.currentTarget.text == "Sign-Up")
+    $(".Sign-Up").removeAttr('hidden')
+    if(e.currentTarget.text == "Sign-In")
+    $(".limiter").removeAttr('hidden')
+    
+    })
+    //validation function for sign up form
+$("#formsubmit").on('submit',function(e){
+  var checkValid  = true;
+  var passwordValue = $("input[name='psw']" ).val();
+  var usernameValue = $("input[name='username']" ).val();
+  var fnameValue = $("input[name='firstname']").val();
+  var lnameValue = $("input[name='lastname']").val();
+  var emailValue = $("input[name='email']").val();
+  var passRegex = new RegExp(/^(?=.*[a-zA-Z])(?=.*[0-9])/);
+  if (passwordValue.length<8 || !passRegex.test(passwordValue) ){
+    checkValid = false;
+    alert("Your password is incorrect - Make sure your password length is minimum 8 and contains letters and numbers")
+  }
+  if(/\d/.test(fnameValue) || /\d/.test(lnameValue)){
+    checkValid = false;
+    alert("First and last name cannot contain numbers");
+  }
+  if(!validateEmail(emailValue)){
+    checkValid = false;
+    alert("Email address is not valid - Must be in the format xxx@xxx.xxx")
+  }
+  if(!checkValid){
+    e.preventDefault();
+  }
+  else{
+    logins.push({
+      key:   usernameValue,
+      value: passwordValue
+    });
+    alert("Successfuly Signed Up!")
+    console.log(logins);
+  }
+})
 });
+
+//validation function for email adress format
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+  //login validation
+  $("#login").on('submit',function(e){
+    var check = true;
+    var usernameValue = $("input[name='username']" ).val();
+    var passwordValue = $("input[name='pass']" ).val();
+
+    if(!logins.includes(usernameValue)){
+      check =false;
+      alert("Username is incorrect")
+    }
+    else{ 
+    if(logins[usernameValue] != passwordValue){
+    check =false;
+    alert("Password is incorrect")
+    }
+  }
+    if(!check){
+      e.preventDefault();
+    }
+    else
+    alert("enjoy your game!")
+  
+  }
+  );
+ 
