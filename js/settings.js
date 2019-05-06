83421f7e1745ae6d0a70984c71af941d8440da57
Nir Kutsky <nirk2012@gmail.com>
2019-05-06 23:08:40 +0300

    fixed bug num2

--- js/settings.js
+++ js/settings.js
@@ -3,88 +3,92 @@ var values = {};
 $(document).ready(function () {
 
     // listen to key down on input and record the key to the input
-$(".keys").on("keydown",function(e){
-   this.value = e.code
-});
-
-// Random generator for settings form in input fields
-$("#btn_rand").on('click',function(){
-    $("form#settingform :input").each(function(){
-        var input = $(this);
-        if(input[0].name=="up")
-            this.value = 'ArrowUp'
-        if(input[0].name=="down")
-            this.value = 'ArrowDown'
-        if(input[0].name=="left")
-            this.value = 'ArrowLeft'
-        if(input[0].name=="right")
-            this.value = 'ArrowRight'
-        if(input[0].name=="ballsnum")
-            this.value = getRandomInt(50,90)
-        if(input[0].name=="ball5color")
-            this.value = hexGenerator()
-        if(input[0].name=="ball15color")
-            this.value = hexGenerator()
-        if(input[0].name=="ball25color")
-            this.value = hexGenerator()
-        if(input[0].name=="time")
-            this.value = getRandomInt(60,360)
-        if(input[0].name=="monstersnum")
-            this.value = getRandomInt(1,3)
-})
-})
-
-$("#settingform").on('submit', function (e) {
-    var keysVal = [];
-    e.preventDefault();
-    keys = $(".keys")
-    keys.each(i => {
-        keysVal.push(keys[i].value);
+    $(".keys").on("keydown", function (e) {
+        this.value = e.code
     });
-    if(!checkIfArrayIsUnique(keysVal)){
-        showAlert("Cannot define 2 or more keys with the same value","Oops..","error");
-    }
-    var $inputs = $('#settingform :input');
 
-    // get an associative array of the values
-    $inputs.each(function() {
-        values[this.name] = $(this).val();
+    // Random generator for settings form in input fields
+    $("#btn_rand").on('click', function () {
+        $("form#settingform :input").each(function () {
+            var input = $(this);
+            if (input[0].name == "up")
+                this.value = 'ArrowUp'
+            if (input[0].name == "down")
+                this.value = 'ArrowDown'
+            if (input[0].name == "left")
+                this.value = 'ArrowLeft'
+            if (input[0].name == "right")
+                this.value = 'ArrowRight'
+            if (input[0].name == "ballsnum")
+                this.value = getRandomInt(50, 90)
+            if (input[0].name == "ball5color")
+                this.value = hexGenerator()
+            if (input[0].name == "ball15color")
+                this.value = hexGenerator()
+            if (input[0].name == "ball25color")
+                this.value = hexGenerator()
+            if (input[0].name == "time")
+                this.value = getRandomInt(60, 360)
+            if (input[0].name == "monstersnum")
+                this.value = getRandomInt(1, 3)
+        })
+    })
+
+    $("#settingform").on('submit', function (e) {
+        var keysVal = [];
+        var checked = true;
+        e.preventDefault();
+        keys = $(".keys")
+        keys.each(i => {
+            keysVal.push(keys[i].value);
+        });
+        if (!checkIfArrayIsUnique(keysVal)) {
+            checked = false;
+            showAlert("Cannot define 2 or more keys with the same value", "Oops..", "error");
+        }
+        if (checked) {
+            var $inputs = $('#settingform :input');
+
+            // get an associative array of the values
+            $inputs.each(function () {
+                values[this.name] = $(this).val();
+            });
+            values["user"] = getLoggedUser();
+            Start(values);
+        }
     });
-    values["user"] = getLoggedUser();
-    Start(values);
-});
 });
 
 function checkIfArrayIsUnique(myArray) {
     return myArray.length === new Set(myArray).size;
-  }
+}
 
-  function getRandomInt(min, max) {
+function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
 
-  function randomHex() {
+function randomHex() {
     var hexNumbers = [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F'
-]
+        0,
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F'
+    ]
 
     // picking a random item of the array
-  return hexNumbers[Math.floor(Math.random() * hexNumbers.length)];
+    return hexNumbers[Math.floor(Math.random() * hexNumbers.length)];
 }
 
 
@@ -98,10 +102,10 @@ function hexGenerator() {
     return hexValue.join('');
 }
 
-function showAlert(msg,header,alertType){
+function showAlert(msg, header, alertType) {
     Swal.fire({
-      type: alertType,
-      title: header,
-      text: msg,
+        type: alertType,
+        title: header,
+        text: msg,
     })
-  }
+}
